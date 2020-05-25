f2b407a884707589369823ece498d50d8637640e
Rehan Ayub <rehanayub20@gmail.com>
2020-05-24 22:57:32 -0400

    Refactor code

--- app.js
+++ app.js
@@ -22,26 +22,23 @@ const dealerCard = document.getElementById('dealerCard');
 let hands = document.querySelectorAll('.hand');
 let cardsList = document.querySelectorAll('.card');
 const playingDeck = document.querySelector('.playingDeck');
-//let hands = playingDeck.childNodes;
-//console.log(hands); 
 
 function traverseDeck(){
     if (typeof traverseDeck.counter == 'undefined' || traverseDeck.counter > 50) {
         traverseDeck.counter = 27;
     }
     traverseDeck.counter ++;
-    //console.log(traverseDeck.counter - 28);
+    
     let currentCard = cards[traverseDeck.counter];
     dealerCard.src = `cards/${currentCard}.svg`;
 } //static counter
 
 deck.addEventListener('click', traverseDeck);
 
-const layoutDeck = () => {
+const layDeck = () => {
     let i = 1, index = 0;
     for (hand of hands) {
         let attributes = hand.getAttribute('data-hand').split(';');
-        //console.log(attributes);
         let obj = {};
         attributes.forEach(str => {
             let keyValPair = str.split(":");
@@ -62,6 +59,25 @@ const layoutDeck = () => {
     }
 }
 
+const layoutDeck = () => {
+    let index = 0;
+    for(let i = 1; i <= 7; i++) {
+        let hand = document.createElement('div');
+        hand.className = 'hand';
+        
+        let cardsArray = [];
+        for(let j = 0; j < i; j++) {
+            if (j === i - 1) cardsArray.push(cards[index]);
+            else cardsArray.push("BLUE_BACK");
+            index++;
+        }
+
+        hand.setAttribute('data-hand', `flow: vertical; spacing: 0.2; cards:${cardsArray.join(',')} `);
+        playingDeck.appendChild(hand);
+    }
+}
+
+
 //Fisher-Yates shuffle algorithm
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -74,97 +90,22 @@ function shuffle(array) {
       [array[i], array[j]] = [array[j], array[i]];
     }
 
-    //console.log(array);
     return array;
   }
 
 const dragEnd = e => {
-    //console.log(hands);
+    
     let source = e.target.src.split("/").slice(-1).pop().split(".")[0];
-    //console.log(source);
-    //console.log(loc);
     let destination = loc.getAttribute('data-hand').split(":").slice(-1).pop().split(",");
-    //console.log(destination.pop());
     destination.push(source);
-    // console.log(loc);
-    // console.log(hands);
-    if (!Array.isArray(hands)) {
-        index = Array.from(hands).indexOf(loc);
-        let newCard = document.createElement('img');
-        newCard.className = 'card';
-        newCard.src = `http://127.0.0.1:5500/cards/${source}.svg`;
-        hands[index].appendChild(newCard);
-        console.log(hands[index]);
-    } else {
-        for (str of hands) {
-            if (str.includes(loc)) index = hands.indexOf(str);
-            //console.log(hands[index]);
-            let temp = hands[index].split("</");
-            // hands[index] = hands[index].split("</");
-            temp[0] += `<img class=\"card\" src=\"http://127.0.0.1:5500/cards/${source}.svg\></`;
-            hands[index] = temp.join('');
-        }
-    }
-    //let index = Array.isArray(hands) ? hands.indexOf(loc) : Array.from(hands).indexOf(loc);
-    //console.log(index);
-    //console.log(hands[index].getAttribute('data-hand'));
-    //hands[index].setAttribute('data-hand', `flow:vertical;spacing:0.2;cards:${destination.join(",")}`);
-    //hands[index].appendChild(`<img src="cards/${source}.svg" alt="" class="card"></img>`);
-    //console.log(typeof hands);
-    //console.log(Array.from(hands));
-    //console.log(hands[index].getAttribute('data-hand'));
-    //layoutDeck();
-    //hands[index].children.append(`<img src="cards/${source}.svg" alt="" class="card"></img>`);
-    //let poi = hands[index].children;
-    // let newCard = document.createElement('img');
-    // newCard.className = 'card';
-    // newCard.src = `http://127.0.0.1:5500/cards/${source}.svg`;
-    // hands[index].appendChild(newCard);
-    //console.log(hands[index]);
-    playingDeck.innerHTML = "";
-    for (hand of hands) {
-        //console.log(typeof hand);
-        if(typeof hand === 'string' || hand instanceof String) {
-            playingDeck.innerHTML += hand;
-            //console.log(playingDeck.innerHTML);
-        } else {
-            playingDeck.innerHTML += hand.outerHTML + "&";
-            //console.log(playingDeck.innerHTML);
-            hands = playingDeck.innerHTML.split("&amp;");
-            playingDeck.innerHTML = playingDeck.innerHTML.replace(/&amp;/g,'');
-        }
-        
-    }
-    //console.log(playingDeck.innerHTML);
-    
-    //console.log(hands);
-    
-    //console.log(hands);
-    //console.log(poi);
-    // let arr = Array.from(poi);
-    // //console.log(arr);
-    // //let newKey = Object.keys(poi).length;
-    // //console.log(newKey);
-    // arr.push(newCard);
-    // let outStr = '';
-    // for(item of arr) {
-    //     outStr += item.outerHTML;
-    // }
-    // console.log(outStr);
-    //console.log(playingDeck.innerHTML);
-    //playingDeck.innerHTML = "";
-    //console.log(playingDeck.innerHTML);
-    //playingDeck.innerHTML.replace(destination.pop(),destination.pop() + "," + source);
-    //console.log(typeof playingDeck.innerHTML);
-    //console.log(playingDeck.innerHTML);
 } //returns source card
 
 const dragEnter = (e) => {
     if (e.target.classList.contains("card")) {
         loc = e.target.parentElement;
-        //console.log(loc);
+        console.log(loc);
     }
-}
+} //makes loc the target card
 
 //playingDeck.addEventListener('dragstart', dragStart);
 playingDeck.addEventListener('dragend', dragEnd);
